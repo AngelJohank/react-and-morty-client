6fa8b054b6e568a8fdf1f5e1ec96b060db68eafa
Angel Johank <angeljohank@gmail.com>
2022-07-21 23:14:56 -0600

    added a simple navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <CharacterList />
     </div>
   );

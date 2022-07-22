dffcc497dbdf10aae19c5541f05b7fb2615dd103
Angel Johank <angeljohank@gmail.com>
2022-07-21 23:36:58 -0600

    styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Link } from "@chakra-ui/react";
 
 function Navbar() {
   return (
-    <Flex align="center" justify="space-between" height="16" mx='8'>
-      <Heading>Hello World</Heading>
+    <Flex align="center" justify="space-between" height="16" mx="8">
+      <Heading fontSize="large">React and Morty</Heading>
+      <div>
+        <Link mr="4">Github</Link>
+        <Link>Api</Link>
+      </div>
     </Flex>
   );
 }
